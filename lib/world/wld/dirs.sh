84237ecc8def0ad80fb830930c784579ebb105d5
Forneck <felipedaniel1984@gmail.com>
2020-10-29 17:57:46 +0000

    zon 92 dirs fix

--- lib/world/wld/dirs.sh
+++ lib/world/wld/dirs.sh
@@ -2,56 +2,56 @@ echo "Removendo ~ com espaço x"
 find ./* -type f -exec sed -i 's/~/~/g' {} \;
 find ./* -type f -exec sed -i 's/~/~/g' {} \;
 echo "feito!"
-echo "Removendo D N"
-find ./* -type f -exec sed -i 's/D N/D 0/g' {} \;
-echo "Removendo DN"
-find ./* -type f -exec sed -i 's/DN/D 0/g' {} \;
+echo "Removendo D 0"
+find ./* -type f -exec sed -i 's/D 0/D 0/g' {} \;
+echo "Removendo D 0"
+find ./* -type f -exec sed -i 's/D 0/D 0/g' {} \;
 echo "feito!"
-echo "Removendo D E"
-find ./* -type f -exec sed -i 's/D E/D 1/g' {} \;
-echo "Removendo DE"
-find ./* -type f -exec sed -i 's/DE/D 1/g' {} \;
+echo "Removendo D 1"
+find ./* -type f -exec sed -i 's/D 1/D 1/g' {} \;
+echo "Removendo D 1"
+find ./* -type f -exec sed -i 's/D 1/D 1/g' {} \;
 echo "feito!"
-echo "Removendo D S"
-find ./* -type f -exec sed -i 's/D S/D 2/g' {} \;
-echo "Removendo DS"
-find ./* -type f -exec sed -i 's/DS/D 2/g' {} \;
+echo "Removendo D 2"
+find ./* -type f -exec sed -i 's/D 2/D 2/g' {} \;
+echo "Removendo D 2"
+find ./* -type f -exec sed -i 's/D 2/D 2/g' {} \;
 echo "feito!"
-echo "Removendo D W"
-find ./* -type f -exec sed -i 's/D W/D 3/g' {} \;
-echo "Removendo DW"
-find ./* -type f -exec sed -i 's/DW/D 3/g' {} \;
+echo "Removendo D 3"
+find ./* -type f -exec sed -i 's/D 3/D 3/g' {} \;
+echo "Removendo D 3"
+find ./* -type f -exec sed -i 's/D 3/D 3/g' {} \;
 echo "feito!"
-echo "Removendo D U"
-find ./* -type f -exec sed -i 's/D U/D 4/g' {} \;
-echo "Removendo DU"
-find ./* -type f -exec sed -i 's/DU/D 4/g' {} \;
+echo "Removendo D 4"
+find ./* -type f -exec sed -i 's/D 4/D 4/g' {} \;
+echo "Removendo D 4"
+find ./* -type f -exec sed -i 's/D 4/D 4/g' {} \;
 echo "feito!"
-echo "Removendo D D"
-find ./* -type f -exec sed -i 's/D D/D 5/g' {} \;
-echo "Removendo DD"
-find ./* -type f -exec sed -i 's/DD/D 5/g' {} \;
+echo "Removendo D 5"
+find ./* -type f -exec sed -i 's/D 5/D 5/g' {} \;
+echo "Removendo D 5"
+find ./* -type f -exec sed -i 's/D 5/D 5/g' {} \;
 echo "feito!"
-echo "Removendo D NW"
-find ./* -type f -exec sed -i 's/D NW/D 6/g' {} \;
-echo "Removendo D NW"
-find ./* -type f -exec sed -i 's/D NW/D 6/g' {} \;
+echo "Removendo D 0W"
+find ./* -type f -exec sed -i 's/D 0W/D 6/g' {} \;
+echo "Removendo D 0W"
+find ./* -type f -exec sed -i 's/D 0W/D 6/g' {} \;
 echo "feito!"
-echo "Removendo D NE"
-find ./* -type f -exec sed -i 's/D NE/D 7/g' {} \;
-echo "Removendo D NE"
-find ./* -type f -exec sed -i 's/D NE/D 7/g' {} \;
+echo "Removendo D 0E"
+find ./* -type f -exec sed -i 's/D 0E/D 7/g' {} \;
+echo "Removendo D 0E"
+find ./* -type f -exec sed -i 's/D 0E/D 7/g' {} \;
 echo "feito!"
-echo "Removendo D SE"
-find ./* -type f -exec sed -i 's/D SE/D 8/g' {} \;
-echo "Removendo DSE"
-find ./* -type f -exec sed -i 's/DSE/D 8/g' {} \;
+echo "Removendo D 2E"
+find ./* -type f -exec sed -i 's/D 2E/D 8/g' {} \;
+echo "Removendo D 2E"
+find ./* -type f -exec sed -i 's/D 2E/D 8/g' {} \;
 echo "feito!"
-echo "Removendo D SW"
-find ./* -type f -exec sed -i 's/D SW/D 9/g' {} \;
-echo "Removendo DSW"
-find ./* -type f -exec sed -i 's/DSW/D 9/g' {} \;
+echo "Removendo D 2W"
+find ./* -type f -exec sed -i 's/D 2W/D 9/g' {} \;
+echo "Removendo D 2W"
+find ./* -type f -exec sed -i 's/D 2W/D 9/g' {} \;
 echo "feito!"
 echo "Alterando Cores"
-find ./* -type f -exec sed -i 's/&/@/g' {} \;
+find ./* -type f -exec sed -i 's/@/@/g' {} \;
 echo "feito!"
