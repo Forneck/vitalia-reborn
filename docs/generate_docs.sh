7b963e0fc94d495fee63ede9a71f699adbb895a1
Forneck <felipedaniel1984@gmail.com>
2021-03-17 18:23:30 +0000

    push

--- docs/generate_docs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_PATH=`dirname $0`
-
-${SCRIPT_PATH}/NaturalDocs-1.52/NaturalDocs -r -i ${SCRIPT_PATH}/../src/include/ -o HTML ~/storage/shared/Documents/html -p ${SCRIPT_PATH}/prj
