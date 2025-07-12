c7f87d373e4ec84b595ba594e477afc255beb29c
Fabiana <felipedaniel1984@gmail.com>
2025-07-11 23:10:03 -0300

    typo no stat para trade arrumado

--- src/act.wizard.c
+++ src/act.wizard.c
@@ -911,7 +911,7 @@ static void do_stat_character(struct char_data *ch, struct char_data *k)
 			 CCCYN(ch, C_NRM), GET_GENGROUP(k), CCNRM(ch, C_NRM));
 	    send_to_char(ch, "Prevalencia de Bravura (Genética): [%s%d%s]\r\n",
 			 CCCYN(ch, C_NRM), GET_GENBRAVE(k), CCNRM(ch, C_NRM));
-	    send_to_char(ch, "Tendência Grupo (Genética): [%s%d%s]\r\n",
+	    send_to_char(ch, "Tendência Trade (Genética): [%s%d%s]\r\n",
 			 CCCYN(ch, C_NRM), GET_GENTRADE(k), CCNRM(ch, C_NRM));
 	    /* Futuramente, podemos adicionar outros genes aqui. */
 	}
